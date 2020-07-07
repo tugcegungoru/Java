19869b35d298ed6bb303c386dfb0401564c03853
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-07-07 18:36:22 +0300

    updated

---
+++ SesliHarfler/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    char harf = 'o';
+	    char[] inceSesli = {'e','i','ö','ü','E','İ','Ö','Ü'};
+        char[] kalinSesli = {'a','ı','o','u','A','I','O','U'};
+        boolean sesliMi = false;
+        for (char h1:inceSesli) {
+            if (h1 == harf) {
+                System.out.println("Harf ince seslidir.");
+                sesliMi =true;
+                return;
+            }
+        }
+
+        for (char h2:kalinSesli) {
+            if (h2 == harf) {
+                System.out.println("Harf kalin seslidir.");
+                sesliMi=true;
+                return;
+            }
+        }
+        if(!sesliMi){
+            System.out.println("Harf sessizdir.");
+        }
+    }
+}
