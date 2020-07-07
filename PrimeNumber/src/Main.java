137c2afe10cfc422d119ebc81af19b7e5e7f1e13
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-07-07 18:15:55 +0300

    updated

--- PrimeNumber/src/Main.java
+++ PrimeNumber/src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        int number = 5;
+        int number = 19;
         int counter = 0;
 
+        if(number == 1){
+            System.out.println("Number is not prime");
+            return;
+        }
+
+        if(number < 1){
+            System.out.println("Number is invalid");
+            return;
+        }
         for(int i=2;i<number/2;i++){
             if(number % i == 0){
                 counter = 1;
