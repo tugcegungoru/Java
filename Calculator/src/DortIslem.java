ba3ec4ed17197b2fc99f9e9efc12b335f6bc1a85
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-07-08 17:14:53 +0300

    uploaded

---
+++ Calculator/src/DortIslem.java
@@ -0,0 +1,15 @@
+public class DortIslem {
+
+    public int Toplama(int sayi1, int sayi2){
+        return sayi1+sayi2;
+    }
+    public int Çıkarma(int sayi1, int sayi2){
+        return sayi1-sayi2;
+    }
+    public int Çarpma(int sayi1, int sayi2){
+        return sayi1*sayi2;
+    }
+    public int Bölme(int sayi1, int sayi2){
+        return sayi1/sayi2;
+    }
+}
